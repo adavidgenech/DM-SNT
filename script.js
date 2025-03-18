2cc29d8d69ac85343a421a9ff8023198cbada3cc
adavidgenech <vidalada@institutdegenech.fr>
2025-03-18 12:47:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function alphaDetect() {
-    let video = document.createElement("video");
-    return video.canPlayType('video/webm; codecs="vp9"') !== "";
-}
+document.addEventListener("DOMContentLoaded", function () {
+    function alphaDetect() {
+        let video = document.createElement("video");
+        return video.canPlayType('video/webm; codecs="vp9"') !== "";
+    }
 
-if (!alphaDetect()) {
-    document.getElementById("creep").style.display = "none";
-}
+    if (!alphaDetect()) {
+        let creepVideo = document.getElementById("creep");
+        if (creepVideo) {
+            creepVideo.style.display = "none";
+        }
+    }
+});
+
