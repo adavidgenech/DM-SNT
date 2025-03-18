440f41df95fbf090231b476a58bc12faf0d6de91
adavidgenech <vidalada@institutdegenech.fr>
2025-03-18 16:03:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let creepVideo = document.getElementById("creep");
-        if (creepVideo) {
-            creepVideo.style.display = "none";
-    }
-});
 
