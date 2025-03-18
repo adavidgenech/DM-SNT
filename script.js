cc76c1486c3aadf8f23ba34261fd214b607ab36b
adavidgenech <vidalada@institutdegenech.fr>
2025-03-18 12:49:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function alphaDetect() {
-        let video = document.createElement("video");
-        return video.canPlayType('video/webm; codecs="vp9"') !== "";
-    }
-
-    if (!alphaDetect()) {
-        let creepVideo = document.getElementById("creep");
+    let creepVideo = document.getElementById("creep");
         if (creepVideo) {
             creepVideo.style.display = "none";
-        }
     }
 });
 
